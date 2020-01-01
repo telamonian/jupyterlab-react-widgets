83d28cb16a4872e70bfb86f73f4790e117752590
telamonian <mklein@jhu.edu>
2019-12-31 22:38:28 -0500

    fixed svg loader in storybook's webpack

--- src/icons.stories.tsx
+++ src/icons.stories.tsx
@@ -12,17 +12,24 @@ import '@jupyterlab/application/style/index.css';
 import '@jupyterlab/theme-light-extension/style/index.css';
 
 const buildIcon = () => ( 
-  <DefaultIconReact name={'build'} />
-)
+  <DefaultIconReact name={'build'} kind={'sideBar'} />
+);
+
+const runningIcon = () => (
+  <DefaultIconReact name={'running'} height={'800px'} width={'800px'} />
+);
 
 const html5Icon = () => (
-  <div className={'foobar'}>
-    <DefaultIconReact name={'html5'} kind={'launcherSection'} center={true} />
+  <div className={'foobar'} style={{height: '500px', width: '500px'}}>
+    <DefaultIconReact name={'html5'} center={true} />
   </div>
 );
 
 storiesOf('Icons', module)
   .add('buildIcon', buildIcon);
+
+storiesOf('Icons', module)
+  .add('runningIcon', runningIcon);
 
 storiesOf('Icons', module)
   .add('html5Icon', html5Icon);
